5573fd4e4edce6053814982e4187e530ed1e6dbd
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-06 14:15:08 +0200

    Class 194

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,6 +12,9 @@ export class LoginComponent implements OnInit {
 
   public page_title: string;
   public user: User;
+  public status: string;
+  public identity;
+  public token;
 
   constructor(
     private _userService: UserService
@@ -24,7 +27,37 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(form) {
-    console.log(this.user);
+    // Conseguir el objeto completo de usuario logueado
+    this._userService.signup(this.user).subscribe(
+      response => {
+        if (response.user && response.user._id) {
+          // Guardamos el usuario en una propiedad
+          this.identity = response.user;
+
+          // Conseguir el token del usuario identificado
+          this._userService.signup(this.user, true).subscribe(
+            response => {
+              if (response.token) {
+                // Guardamos el token en una propiedad
+                this.token = response.token;
+              } else {
+                this.status = 'error';
+              }
+            },
+            error => {
+              this.status = 'error';
+              console.log(error);
+            }
+          );
+        } else {
+          this.status = 'error';
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log(error);
+      }
+    );
   }
 
 }
