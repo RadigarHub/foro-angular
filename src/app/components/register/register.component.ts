fe6c141d49ff4466eb79854ba6db3490346f54d4
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-06 13:07:05 +0200

    Class 190

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
+import { UserService } from '../../services/user.services';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
 
   public page_title: string;
   public user: User;
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.page_title = "Regístrate";
     this.user = new User('', '', '', '', '', '', 'ROLE_USER');
   }
 
   ngOnInit(): void {
+    console.log(this._userService.prueba());
   }
 
   onSubmit(form) {
