bd8e664439b207de721009514f68c37423598459
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-06 13:13:14 +0200

    Class 191

--- src/app/services/user.services.ts
+++ src/app/services/user.services.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { global } from './global';
 import { User } from '../models/user';
 
 @Injectable()
 export class UserService {
-  constructor(private _http: HttpClient) {
 
+  public url: string;
+
+  constructor(private _http: HttpClient) {
+    this.url = global.url;
   }
 
   prueba() {
     return "Hola mundo desde el servicio de Angular";
+  }
+
+  register(user): Observable<any> {
+    // Convertir el objeto del usuario a un json string
+    let params = JSON.stringify(user);
+
+    // Definir las cabeceras
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    // Hacer petición AJAX
+    return this._http.post(this.url + 'register', params, {headers: headers});
   }
 }
