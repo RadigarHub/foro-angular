2eca2bb3c404085c024c6be2c954346018c6e97f
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-07 16:05:14 +0200

    Class 200 and 201

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.services';
+import { global } from '../../services/global';
 
 @Component({
   selector: 'app-user-edit',
@@ -16,6 +17,8 @@ export class UserEditComponent implements OnInit {
   public idendity: Object;
   public token: string;
   public status: string;
+  public url: string;
+  public afuConfig;
 
   constructor(
     private _router: Router,
@@ -26,9 +29,33 @@ export class UserEditComponent implements OnInit {
     this.idendity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     this.user = <User>this.idendity;
+    this.url = global.url;
+
+    // Configuración del angular file uploader. 
+    // +info en https://www.npmjs.com/package/angular-file-uploader
+    this.afuConfig = {
+      multiple: false,
+      formatAllowed: ".jpg, .jpeg, .png, .gif",
+      uploadAPI:{
+        url: this.url + "upload-avatar",
+        headers: {
+          "Authorization": this.token
+        }
+      },
+      theme: "attachPin",
+      hideProgressBar: false,
+      hideResetBtn: true,
+      hideSelectBtn: false,
+      attachPinText: "Sube tu foto"
+    };
   }
 
   ngOnInit(): void {
+  }
+
+  avatarUpload(data) {
+    this.user.image = data.body.user.image;
+    console.log(this.user);
   }
 
 }
