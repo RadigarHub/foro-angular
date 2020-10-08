349b919a95c74a3c40e80824407c8767735010ff
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-08 11:08:17 +0200

    Class 212

---
+++ src/app/services/topic.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { global } from './global';
+
+@Injectable()
+export class TopicService {
+
+  public url: string;
+
+  constructor(
+    private _http: HttpClient
+  ) {
+    this.url = global.url;
+  }
+
+  prueba() {
+    return "hola mundo desde el topic service";
+  }
+}
