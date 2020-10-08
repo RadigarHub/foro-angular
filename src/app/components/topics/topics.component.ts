f37159e04e2574378f6c14ae5c7bc62613979647
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-08 13:33:30 +0200

    Class 222

--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Topic } from '../../models/topic';
+import { TopicService } from '../../services/topic.service';
 
 @Component({
   selector: 'app-topics',
   templateUrl: './topics.component.html',
-  styleUrls: ['./topics.component.css']
+  styleUrls: ['./topics.component.css'],
+  providers: [TopicService]
 })
 export class TopicsComponent implements OnInit {
 
-  constructor() { }
+  public page_title: string;
+  public topics: Topic[];
+  public totalPages: number;
+  public page: number;
+  public next_page: number;
+  public pre_page: number;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _topicService: TopicService
+  ) {
+    this.page_title = "Temas";
+  }
 
   ngOnInit(): void {
   }
