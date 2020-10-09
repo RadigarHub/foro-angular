bc61382f3d28bda9e552a3b704c91656440309c8
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-09 11:54:59 +0200

    Class 229

--- src/app/components/topic-detail/topic-detail.component.ts
+++ src/app/components/topic-detail/topic-detail.component.ts
@@ -2,22 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Topic } from '../../models/topic';
 import { TopicService } from '../../services/topic.service';
+import { UserService } from '../../services/user.service';
+import { Comment } from '../../models/comment';
 
 @Component({
   selector: 'app-topic-detail',
   templateUrl: './topic-detail.component.html',
   styleUrls: ['./topic-detail.component.css'],
-  providers: [TopicService]
+  providers: [TopicService, UserService]
 })
 export class TopicDetailComponent implements OnInit {
 
   public topic: Topic;
+  public comment: Comment;
+  public identity;
+  public token: string;
+  public status: string;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _topicService: TopicService
-  ) { }
+    private _topicService: TopicService,
+    private _userService: UserService
+  ) {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.comment = new Comment('', '', '', this.identity._id)
+  }
 
   ngOnInit(): void {
     this.getTopic();
