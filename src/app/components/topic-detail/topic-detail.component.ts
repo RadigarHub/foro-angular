fea9af7753cd89508e79025ba5001fff6da7ab13
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-09 12:40:46 +0200

    Class 235

--- src/app/components/topic-detail/topic-detail.component.ts
+++ src/app/components/topic-detail/topic-detail.component.ts
@@ -5,6 +5,7 @@ import { TopicService } from '../../services/topic.service';
 import { UserService } from '../../services/user.service';
 import { CommentService } from '../../services/comment.service';
 import { Comment } from '../../models/comment';
+import { global } from '../../services/global';
 
 @Component({
   selector: 'app-topic-detail',
@@ -19,6 +20,7 @@ export class TopicDetailComponent implements OnInit {
   public identity;
   public token: string;
   public status: string;
+  public url: string;
 
   constructor(
     private _route: ActivatedRoute,
@@ -29,7 +31,8 @@ export class TopicDetailComponent implements OnInit {
   ) {
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
-    this.comment = new Comment('', '', '', this.identity._id)
+    this.comment = new Comment('', '', '', this.identity._id);
+    this.url = global.url;
   }
 
   ngOnInit(): void {
@@ -64,6 +67,23 @@ export class TopicDetailComponent implements OnInit {
           this.status = "success";
           this.topic = response.topic;
           form.reset();
+        }
+      },
+      error => {
+        console.log(error);
+        this.status = "error";
+      }
+    );
+  }
+
+  deleteComment(id) {
+    this._commentService.delete(this.token, this.topic._id, id).subscribe(
+      response => {
+        if (!response.topic) {
+          this.status = "error";
+        } else {
+          this.status = "success";
+          this.topic = response.topic;
         }
       },
       error => {
